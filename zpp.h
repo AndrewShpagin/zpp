053e4a5eaf51b666465220eda2e2c124b343eaf5
Andrew <andrewshpagin@gmail.com>
2021-08-29 20:17:45 +0300

    documenting

--- zpp.h
+++ zpp.h
@@ -32,6 +32,7 @@ public:
 	bool successful();
 };
 
+/// The simple c++ interface to extract ZIP files
 class zipFileReader {
 	mz_zip_archive ar;
 	bool errors;
@@ -40,74 +41,20 @@ class zipFileReader {
 	}
 	void createPathForFile(const std::string&);
 public:
-	zipFileReader(const std::string& archiveName) {
-		memset(&ar, 0, sizeof(ar));
-		errors = !mz_zip_reader_init_file(&ar, archiveName.c_str(), 0);
-	}
-	~zipFileReader() {
-		mz_zip_reader_end(&ar);
-	}
-	std::vector<std::string> getFilesList() {
-		std::vector<std::string> list;
-		size_t n = mz_zip_reader_get_num_files(&ar);
-		for(int i=0;i<n;i++) {
-			if (mz_zip_reader_is_file_encrypted(&ar, i))continue;
-			mz_zip_archive_file_stat file_stat;
-			if(mz_zip_reader_file_stat(&ar, i, &file_stat)) {
-				char name[2048];
-				mz_zip_reader_get_filename(&ar, i, name, sizeof(name));
-				list.push_back(name);
-			}
-		}
-		return list;
-	}
-	void extractToFolder(const std::string& nameInArchive, const std::string& destFolder) {
-		int idx = getIndex(nameInArchive);
-		if (idx != -1) {
-			try{
-				std::filesystem::path p = destFolder;
-				p.append(nameInArchive);
-				createPathForFile(p.generic_u8string());
-				mz_zip_reader_extract_to_file(&ar, idx, p.generic_u8string().c_str(), 0);
-			}
-			catch (std::exception e) {
-				std::cout << e.what() << std::endl;
-			}
-		}
-	}
-	void extractToFile(const std::string& nameInArchive, const std::string& destFilename) {
-		int idx = getIndex(nameInArchive);
-		if (idx != -1) {
-			mz_zip_reader_extract_to_file(&ar, idx, destFilename.c_str(), 0);
-		}
-	}
-	void extractAll(const std::string& destFolder) {
-		size_t n = mz_zip_reader_get_num_files(&ar);
-		for (int i = 0; i < n; i++) {
-			if (mz_zip_reader_is_file_encrypted(&ar, i))continue;
-			mz_zip_archive_file_stat file_stat;
-			if (mz_zip_reader_file_stat(&ar, i, &file_stat)) {
-				try{
-					char name[2048];
-					mz_zip_reader_get_filename(&ar, i, name, sizeof(name));
-					std::filesystem::path p = destFolder;
-					p.append(name);
-					createPathForFile(p.generic_u8string());
-					mz_zip_reader_extract_to_file(&ar, i, p.generic_u8string().c_str(), 0);
-				}
-				catch (std::exception e) {
-					std::cout << e.what() << std::endl;
-				}
-			}
-		}
-	}
-	void extractFirstToFile(const std::string& destFilename) {
-		size_t n = mz_zip_reader_get_num_files(&ar);
-		if (n > 0) {
-			createPathForFile(destFilename);
-			mz_zip_reader_extract_to_file(&ar, 0, destFilename.c_str(), 0);
-		}
-	}
+	/// Open the archive
+	zipFileReader(const std::string& archiveName);
+	~zipFileReader();
+
+	/// Get list of files in the archive
+	std::vector<std::string> getFilesList();
+	/// Extract one file from the archive to the destination folder
+	void extractToFolder(const std::string& nameInArchive, const std::string& destFolder);
+	/// Extract one file from the archive to file, name may be different from the filename in the archive
+	void extractToFile(const std::string& nameInArchive, const std::string& destFilename);
+	/// Extract al files to the folder
+	void extractAll(const std::string& destFolder);
+	/// Extract the first file in the archive to the destination filename. It is useful if you have just one file in the archive.
+	void extractFirstToFile(const std::string& destFilename);
 };
 
 inline zipFileWriter::zipFileWriter(const std::string& destArchiveName) {
@@ -174,5 +121,80 @@ inline void zipFileReader::createPathForFile(const std::string& destFilename) {
 	}
 	catch (std::exception e) {
 		std::cout << e.what() << std::endl;
+	}
+}
+
+inline zipFileReader::zipFileReader(const std::string& archiveName) {
+	memset(&ar, 0, sizeof(ar));
+	errors = !mz_zip_reader_init_file(&ar, archiveName.c_str(), 0);
+}
+
+inline zipFileReader::~zipFileReader() {
+	mz_zip_reader_end(&ar);
+}
+
+inline std::vector<std::string> zipFileReader::getFilesList() {
+	std::vector<std::string> list;
+	size_t n = mz_zip_reader_get_num_files(&ar);
+	for (int i = 0; i < n; i++) {
+		if (mz_zip_reader_is_file_encrypted(&ar, i))continue;
+		mz_zip_archive_file_stat file_stat;
+		if (mz_zip_reader_file_stat(&ar, i, &file_stat)) {
+			char name[2048];
+			mz_zip_reader_get_filename(&ar, i, name, sizeof(name));
+			list.push_back(name);
+		}
+	}
+	return list;
+}
+
+inline void zipFileReader::extractToFolder(const std::string& nameInArchive, const std::string& destFolder) {
+	int idx = getIndex(nameInArchive);
+	if (idx != -1) {
+		try {
+			std::filesystem::path p = destFolder;
+			p.append(nameInArchive);
+			createPathForFile(p.generic_u8string());
+			mz_zip_reader_extract_to_file(&ar, idx, p.generic_u8string().c_str(), 0);
+		}
+		catch (std::exception e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+}
+
+inline void zipFileReader::extractToFile(const std::string& nameInArchive, const std::string& destFilename) {
+	int idx = getIndex(nameInArchive);
+	if (idx != -1) {
+		mz_zip_reader_extract_to_file(&ar, idx, destFilename.c_str(), 0);
+	}
+}
+
+inline void zipFileReader::extractAll(const std::string& destFolder) {
+	size_t n = mz_zip_reader_get_num_files(&ar);
+	for (int i = 0; i < n; i++) {
+		if (mz_zip_reader_is_file_encrypted(&ar, i))continue;
+		mz_zip_archive_file_stat file_stat;
+		if (mz_zip_reader_file_stat(&ar, i, &file_stat)) {
+			try {
+				char name[2048];
+				mz_zip_reader_get_filename(&ar, i, name, sizeof(name));
+				std::filesystem::path p = destFolder;
+				p.append(name);
+				createPathForFile(p.generic_u8string());
+				mz_zip_reader_extract_to_file(&ar, i, p.generic_u8string().c_str(), 0);
+			}
+			catch (std::exception e) {
+				std::cout << e.what() << std::endl;
+			}
+		}
+	}
+}
+
+inline void zipFileReader::extractFirstToFile(const std::string& destFilename) {
+	size_t n = mz_zip_reader_get_num_files(&ar);
+	if (n > 0) {
+		createPathForFile(destFilename);
+		mz_zip_reader_extract_to_file(&ar, 0, destFilename.c_str(), 0);
 	}
 }
